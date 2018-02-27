efbc388b51398a0b4ae7dc859efcccc224a84dc4
Eliseo Papa <elipapa@users.noreply.github.com>
2018-02-27 20:13:06 +0000

    use pipenv; circleci vs travis

--- README.md
+++ README.md
@@ -27,6 +27,17 @@ alzheimer's
 DO:124125
 ```
 
+# Developing
+
+## add a dependency with pipenv + setup.py
+To add a dep for a library, add it by hand to `setup.py`, then add it separately to Pipfile, so that it shows up both as a transitive dependency and in your locked dev environment
+
+## Release to PyPi
+1. Once you are ready to cut a new release, update the version in setup.py and create a new git tag with git tag $VERSION.
+2. Once you push the tag to GitHub with git push --tags a new CircleCI build is triggered.
+3. You run a verification step to ensure that the git tag matches the version of ontoma that you added in step 1.
+4. CircleCI performs all tests.
+5. Once all of your test pass, you create a new Python package and upload it to PyPI using twine.
 
 ## TODO:
 
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup, find_packages
 
+VERSION = "0.0.1"
 
 with open('README.md') as f:
     readme = f.read()
@@ -9,15 +10,47 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
 setup(
     name='OnToma',
-    version='0.0.1',
+    version=VERSION,
     description='Ontology mapping for Open Targets',
     long_description=readme,
     author='Open Targets dev team',
     author_email='ops@opentargets.org',
     url='https://github.com/opentargets/OnToma',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs'))
+    packages=find_packages(exclude=('tests', 'docs')),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords='opentargets ontology efo mapper',
+    install_requires=[
+        'requests',
+        'python-jsonschema-objects',
+        'obonet'
+    ],
+    python_requires='>=3',
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
 
