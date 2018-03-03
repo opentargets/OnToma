5568b58ea2b695fc94b50d1bb39d96aaff9158c9
Eliseo Papa <elipapa@users.noreply.github.com>
2018-03-03 01:01:41 +0000

    return a list directly

--- ontoma/ols.py
+++ ontoma/ols.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # borrowed from https://github.com/cthoyt/ols-client/blob/master/src/ols_client/client.py
-# ** Many thanks! **
-# Here expanded for all search and suggest parameters
+# Removed ontology and term methods. 
+# Adding details/parameters for all search methods
 import logging
 import time
 import requests
@@ -17,24 +17,7 @@ logger = logging.getLogger(__name__)
 api_suggest = '/api/suggest'
 api_search = '/api/search'
 api_select = '/api/select'
-# TODO remove below if methods not used
-api_ontology = '/api/ontologies/{ontology}'
-api_terms = '/api/ontologies/{ontology}/terms'
-api_term = '/api/ontologies/{ontology}/terms/{iri}'
-api_properties = '/api/ontologies/{ontology}/properties/{iri}'
-api_indivduals = '/api/ontologies/{ontology}/individuals/{iri}'
-api_descendants = '/api/ontologies/{ontology}/terms/{iri}/hierarchicalDescendants'
 
-HIERARCHICAL_CHILDREN = 'hierarchicalChildren'
-
-def _help_iterate_labels(term_iterator):
-    for term in term_iterator:
-        yield term['label']
-
-def _iterate_response_terms(response):
-    """Iterates over the terms in the given response"""
-    for term in response['_embedded']['terms']:
-        yield term
 
 class OlsClient:
     """Wraps the functions to query the Ontology Lookup Service such that alternative base URL's can be used.
@@ -42,7 +25,11 @@ class OlsClient:
     >>> ols = OlsClient()
     >>> r = ols.search('asthma')
     >>> r['response']['docs'][0]['iri']
-    u'http://purl.obolibrary.org/obo/NCIT_C28397'
+    'http://purl.obolibrary.org/obo/NCIT_C28397'
+
+    >>> r = ols.search('asthma',ontology=['efo'],query_fields=['synonims'],field_list=['iri','label'])
+    >>> r['response']['docs'][0]['iri']
+    'http://purl.obolibrary.org/obo/NCIT_C28397'
 
     >>> r = ols.suggest('asthma', ontology=['efo','ordo','hpo'])
     >>> r['response']['docs'][0]['autosuggest']
@@ -62,39 +49,41 @@ class OlsClient:
         """
         self.base = (ols_base if ols_base is not None else URLS.OLS).rstrip('/')
 
-        self.ontology_terms_fmt = self.base + api_terms
-        self.ontology_term_fmt = self.base + api_term
-        self.ontology_metadata_fmt = self.base + api_ontology
         self.ontology_suggest = self.base + api_suggest
         self.ontology_select = self.base + api_select
         self.ontology_search = self.base + api_search
-        self.ontology_term_descendants_fmt = self.base + api_descendants
 
 
-
-    def get_term(self, ontology, iri):
-        """Gets the data for a given term
-        :param str ontology: The name of the ontology
-        :param str iri: The IRI of a term
-        :rtype: dict
-        """
-        url = self.ontology_term_fmt.format(ontology, iri)
-        response = requests.get(url)
-
-        return response.json()
-
-    def search(self, name, query_fields=None):
+    def search(self, name, query_fields=None, ontology=None, 
+                    field_list=None, facet=False, hl=False):
         """Searches the OLS with the given term
         :param str name:
         :param list[str] query_fields: Fields to query
-        :return: dict
+
+        By default the search is performed over term labels, synonyms, 
+        descriptions, identifiers and annotation properties.
+        Specify the fields to query, the defaults are 
+        {label, synonym, description, short_form, obo_id, annotations, logical_description, iri}
+
+        :param list[str] ontology: list of ontologies id
+        :return: list
         """
         params = {'q': name}
-        if query_fields is not None:
-            params['queryFields'] = '{{{}}}'.format(','.join(query_fields))
-        response = requests.get(self.ontology_search, params=params)
+        params['facet'] = 'true' if facet else 'false'
 
-        return response.json()
+        if ontology:
+            params['ontology'] = ','.join(ontology)
+        if query_fields:
+            params['queryFields'] = ','.join(query_fields)
+        if field_list:
+            params['fieldList'] = ','.join(field_list)
+        r = requests.get(self.ontology_search, params=params)
+        r.raise_for_status()
+        if r.json()['response']['numFound']:
+            return r.json()['response']['docs']
+        else:
+            logger.debug('OLS returned empty response for {}'.format(name))
+            return None
 
     def suggest(self, name, ontology=None):
         """Suggest terms from an optional list of ontologies
@@ -123,124 +112,4 @@ class OlsClient:
 
         return response.json()
 
-    @staticmethod
-    def _iter_terms_helper(url, size=None, sleep=None):
-        """Iterates over all terms, lazily with paging
-        :param str url: The url to query
-        :param int size: The size of each page. Defaults to 500, which is the maximum allowed by the EBI.
-        :param int sleep: The amount of time to sleep between pages. Defaults to none.
-        :rtype: iter[dict]
-        """
-        if size is None:
-            size = 500
-        elif size > 500:
-            raise ValueError('Maximum size is 500. Given: {}'.format(size))
-
-        t = time.time()
-        response = requests.get(url, params={'size': size}).json()
-        links = response['_links']
-
-        for response_term in _iterate_response_terms(response):
-            yield response_term
-
-        t = time.time() - t
-
-        log.info(
-            'Page %s/%s done in %.2f seconds',
-            response['page']['number'] + 1,
-            response['page']['totalPages'],
-            t
-        )
-
-        log.info('Estimated time until done: %.2f minutes', t * response['page']['totalPages'] / 60)
-
-        while 'next' in links:
-            if sleep:
-                time.sleep(sleep)
-
-            t = time.time()
-            response = requests.get(links['next']['href'], params={'size': size}).json()
-            links = response['_links']
-
-            for response_term in _iterate_response_terms(response):
-                yield response_term
-
-            log.info(
-                'Page %s/%s done in %.2f seconds',
-                response['page']['number'],
-                response['page']['totalPages'],
-                time.time() - t
-            )
-
-    def iter_terms(self, ontology, size=None, sleep=None):
-        """Iterates over all terms, lazily with paging
-        :param str ontology: The name of the ontology
-        :param int size: The size of each page. Defaults to 500, which is the maximum allowed by the EBI.
-        :param int sleep: The amount of time to sleep between pages. Defaults to 0 seconds.
-        :rtype: iter[dict]
-        """
-        url = self.ontology_terms_fmt.format(ontology=ontology)
-        for term in self._iter_terms_helper(url, size=size, sleep=sleep):
-            yield term
-
-    def iter_descendants(self, ontology, iri, size=None, sleep=None):
-        """Iterates over the descendants of a given term
-        :param str ontology: The name of the ontology
-        :param str iri: The IRI of a term
-        :param int size: The size of each page. Defaults to 500, which is the maximum allowed by the EBI.
-        :param int sleep: The amount of time to sleep between pages. Defaults to 0 seconds.
-        :rtype: iter[dict]
-        """
-        url = self.ontology_term_descendants_fmt.format(ontology=ontology, iri=iri)
-        log.info('getting %s', url)
-        for term in self._iter_terms_helper(url, size=size, sleep=sleep):
-            yield term
-
-    def iter_descendants_labels(self, ontology, iri, size=None, sleep=None):
-        """Iterates over the labels for the descendants of a given term
-        :param str ontology: The name of the ontology
-        :param str iri: The IRI of a term
-        :param int size: The size of each page. Defaults to 500, which is the maximum allowed by the EBI.
-        :param int sleep: The amount of time to sleep between pages. Defaults to 0 seconds.
-        :rtype: iter[str]
-        """
-        for label in _help_iterate_labels(self.iter_descendants(ontology, iri, size=size, sleep=sleep)):
-            yield label
-
-    def iter_labels(self, ontology, size=None, sleep=None):
-        """Iterates over the labels of terms in the ontology. Automatically wraps the pager returned by the OLS.
-        :param str ontology: The name of the ontology
-        :param int size: The size of each page. Defaults to 500, which is the maximum allowed by the EBI.
-        :param int sleep: The amount of time to sleep between pages. Defaults to 0 seconds.
-        :rtype: iter[str]
-        """
-        for label in _help_iterate_labels(self.iter_terms(ontology=ontology, size=size, sleep=sleep)):
-            yield label
-
-    def iter_hierarchy(self, ontology, size=None, sleep=None):
-        """Iterates over parent-child relations
-        :param str ontology: The name of the ontology
-        :param int size: The size of each page. Defaults to 500, which is the maximum allowed by the EBI.
-        :param int sleep: The amount of time to sleep between pages. Defaults to 0 seconds.
-        :rtype: iter[tuple[str,str]]
-        """
-        for term in self.iter_terms(ontology=ontology, size=size, sleep=sleep):
-            try:
-                hierarchy_children_link = term['_links'][HIERARCHICAL_CHILDREN]['href']
-            except KeyError:  # there's no children for this one
-                continue
-
-            response = requests.get(hierarchy_children_link).json()
-
-            for child_term in response['_embedded']['terms']:
-                yield term['label'], child_term['label']  # TODO handle different relation types
-
-    def get_description(self, ontology):
-        """Gets the description of a given ontology
-        :param str ontology: The name of the ontology
-        :rtype: str
-        """
-        response = self.get_ontology(ontology)
-
-        return response['config'].get('description')
-
+    
