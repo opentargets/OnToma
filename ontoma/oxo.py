c73466751d72104dc795a27fb413c32b598730f6
Eliseo Papa <elipapa@users.noreply.github.com>
2018-03-12 17:37:18 +0000

    wrapper

--- ontoma/oxo.py
+++ ontoma/oxo.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+"""
+wrapper for the OXO api 
+(reverse engineered! argh!)
+"""
+
+
+
 import logging
 import time
 import requests
@@ -15,18 +22,20 @@ logger = logging.getLogger(__name__)
 
 
 class OxoClient:
+    ''' OXO wrapper class
+
+    >>> oxo = OxoClient()
+    >>> oxo.search(input_source="ICD9CM", distance=2)
+    '''
+
     def __init__(self, base_url=OXO.rstrip('/')):
         self._baseapi = base_url
         self._searchapi = base_url + '/search'
         self._mappingsapi = base_url + '/mappings'
     
     def search(self, ids = None, input_source = None, mapping_target = 'EFO', distance = 1):
-        '''search the mappings to EFO
-        >>> oxo = OxoClient()
-        >>> oxo.search(input_source="ICD9CM", distance=2)
-        '''
         payload = {
-            "size": 1000,
+            "size": 500,
             "ids": ids,
             'inputSource': input_source,
             'mappingTarget': mapping_target,
@@ -35,6 +44,15 @@ class OxoClient:
 
         payload={"ids":ids,"inputSource":"ICD9CM","mappingTarget":["EFO"],"distance":"3"}
         r = requests.post(self._searchapi, data=payload)
+
+        #TODO must paginate!
+        #https://www.ebi.ac.uk/spot/oxo/api/search?page=1&size=500
+        # Pagination - if there is a "next" link, update URL and read next page	
+        # try:
+        #     url = r.links['next']['url']
+        # except KeyError:						# If there is no "next" link
+        #     break
+
         return r.json()['_embedded']['searchResults']
         #     while oxo.json().get('next') == True:
         # for row in oxomappings:
