6774a3e90aa622d084c7d92d01cfdfb83ec6461a
Eliseo Papa <elipapa@users.noreply.github.com>
2018-04-19 14:32:28 +0100

    nicely aligned logging

--- ontoma/__init__.py
+++ ontoma/__init__.py
@@ -12,7 +12,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 ch = logging.StreamHandler()
-formatter = logging.Formatter('%(levelname)s - %(name)s - %(message)s')
+formatter = logging.Formatter('%(levelname)-8s - %(name)s - %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
