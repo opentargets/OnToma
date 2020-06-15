b48605a6fa4c9870eceecc40de5101b476028cfd
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2020-06-15 15:35:38 +0100

    Use latest EFO OBO file instead of hard-coded v2018-01-15

--- ontoma/constants.py
+++ ontoma/constants.py
@@ -2,7 +2,7 @@
 
 URLS = {
 
-    'EFO':'https://github.com/EBISPOT/efo/raw/v2018-01-15/efo.obo',
+    'EFO':'https://www.ebi.ac.uk/efo/efo.obo',
     'HP':'http://purl.obolibrary.org/obo/hp.obo',
     'OMIM_EFO_MAP':'https://raw.githubusercontent.com/opentargets/platform_semantic/master/resources/xref_mappings/omim_to_efo.txt',
     'ZOOMA_EFO_MAP':'https://raw.githubusercontent.com/opentargets/platform_semantic/master/resources/zooma/cttv_indications_3.txt',
